53bf0b81e156b781b2077ec4b40309591e328684
navacar <55153165+navacar@users.noreply.github.com>
2021-09-15 11:13:09 +0200

    initial commit

---
+++ EmailSender.cpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c), 2021 nK ind.
+ *
+ * EmailSender - send photo to email.
+ * 
+ * @created 2021-09-15 by Alexandr Navasardyan and Nikita Zubach
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
