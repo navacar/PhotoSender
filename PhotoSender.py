41f9884dd179658452d699a64d6e89da22512055
navacar <55153165+navacar@users.noreply.github.com>
2021-09-21 21:39:50 +0200

    Made receive and start delete
    Program now receive email with code and send on it photo which is suitable to it.
    
    TO DO: make program to delete old emails

--- PhotoSender.py
+++ PhotoSender.py
@@ -1,16 +1,92 @@
+###############################################################################
+# Copyright (c), 2021 nK ind.                                                 #
+#                                                                             #
+#                                                                             #
+# PhotoSender.py - recive email with code and send on it corresponding photo  #
+#                                                                             #
+#                                                                             #
+# @created 2021-09-21 by Alexandr Navasardyan                                 #
+###############################################################################
+
 import smtplib
+import imaplib
+
+import email
+
+from email.header import decode_header
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
+
 from Variables import PASSWORD
 
-def send_test_mail(reciver, imageCode):
-    sender_email = "navacartest@mail.ru"
-    receiver_email = reciver
+emailAdress = "navacartest@mail.ru"
+server = "smtp.mail.ru"
+imapServer = "imap.mail.ru"
+port = 587
 
+def deleteMessage(reciver):
+    imap = imaplib.IMAP4_SSL(imapServer)
+    imap.login(emailAdress, PASSWORD)
+    imap.select('inbox')
+    status, messages = imap.search(None, f'FROM {reciver}')
+    messages = messages[0].split(b' ')
+
+    for mail in messages:
+        msg = imap.fetch(mail, "(RFC822)")
+    
+        for response in msg:
+            if isinstance(response, tuple):
+                msg = email.message_from_bytes(response[1])
+                subject = decode_header(msg["Subject"])[0][0]
+                
+                if isinstance(subject, bytes):
+                    subject = subject.decode()
+                
+                print("Deleting", subject)
+        imap.store(mail, "+FLAGS", "\\Deleted")
+    
+    imap.expunge()
+    imap.close()
+    imap.logout()
+
+
+def receiveEmail():
+    mail = imaplib.IMAP4_SSL(imapServer)
+    mail.login(emailAdress, PASSWORD)
+    mail.select('inbox')
+    
+    status, data = mail.search(None, 'ALL')
+    
+    mail_ids = []
+    for block in data:
+        mail_ids += block.split()
+
+    for i in mail_ids:
+        status, data = mail.fetch(i, '(RFC822)')
+    
+        for response_part in data:
+            
+            if isinstance(response_part, tuple):
+                message = email.message_from_bytes(response_part[1])
+
+                mail_from = message['from']
+                mail_subject = message['subject']
+
+                if message.is_multipart():
+                    mail_content = ''
+                    for part in message.get_payload():
+                        if part.get_content_type() == 'text/plain':
+                            mail_content += part.get_payload()
+                else:
+                    mail_content = message.get_payload()
+
+    return mail_from, mail_content
+
+def send_test_mail(reciver, imageCode):
     msg = MIMEMultipart()
     msg['Subject'] = '[Email Test]'
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
+    msg['From'] = emailAdress
+    msg['To'] = reciver
 
     image = "Resources/" + str(imageCode) + ".jpg"
 
@@ -20,13 +96,15 @@ def send_test_mail(reciver, imageCode):
         msg.attach(img)
 
     try:
-        with smtplib.SMTP('smtp.mail.ru', 587) as smtpObj:
+        with smtplib.SMTP(server, port) as smtpObj:
             smtpObj.ehlo()
             smtpObj.starttls()
-            smtpObj.login("navacartest@mail.ru", PASSWORD)
-            smtpObj.sendmail(sender_email, receiver_email, msg.as_string())
+            smtpObj.login(emailAdress, PASSWORD)
+            smtpObj.sendmail(emailAdress, reciver, msg.as_string())
             smtpObj.quit()
     except Exception as e:
-        print(e)
+        print(e)    
 
-send_test_mail("navacar@mail.ru", 123)
+reciver, code = receiveEmail()
+send_test_mail(reciver, int(code))
+# deleteMessage(reciver)
