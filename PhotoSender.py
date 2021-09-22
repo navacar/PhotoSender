7469762c4e43e2722f565c5f1531fec5ac878ed3
navacar <55153165+navacar@users.noreply.github.com>
2021-09-22 18:27:41 +0200

    Automatized function and added exceptions
    Automatized receiving and sending functions and added exceptions to avoid execution fails

--- PhotoSender.py
+++ PhotoSender.py
@@ -10,8 +10,9 @@
 
 import smtplib
 import imaplib
-
+import time
 import email
+import os
 
 from email.header import decode_header
 from email.mime.multipart import MIMEMultipart
@@ -22,67 +23,51 @@ from Variables import PASSWORD
 emailAdress = "navacartest@mail.ru"
 server = "smtp.mail.ru"
 imapServer = "imap.mail.ru"
-port = 587
+smtpPort = 587
+imapPort = 993
 
-def deleteMessage(reciver):
-    imap = imaplib.IMAP4_SSL(imapServer)
-    imap.login(emailAdress, PASSWORD)
-    imap.select('inbox')
-    status, messages = imap.search(None, f'FROM {reciver}')
-    messages = messages[0].split(b' ')
-
-    for mail in messages:
-        msg = imap.fetch(mail, "(RFC822)")
-    
-        for response in msg:
-            if isinstance(response, tuple):
-                msg = email.message_from_bytes(response[1])
-                subject = decode_header(msg["Subject"])[0][0]
-                
-                if isinstance(subject, bytes):
-                    subject = subject.decode()
-                
-                print("Deleting", subject)
-        imap.store(mail, "+FLAGS", "\\Deleted")
-    
-    imap.expunge()
-    imap.close()
-    imap.logout()
+def deleteMessage():
+    typ, data = mail.search(None, 'SEEN')
+    for num in data[0].split():
+        mail.store(num, '+FLAGS', '\\Deleted')
+    mail.expunge()
+    mail.close()
+    mail.logout()
 
 
 def receiveEmail():
-    mail = imaplib.IMAP4_SSL(imapServer)
-    mail.login(emailAdress, PASSWORD)
-    mail.select('inbox')
-    
     status, data = mail.search(None, 'ALL')
-    
-    mail_ids = []
-    for block in data:
-        mail_ids += block.split()
 
-    for i in mail_ids:
+    mailFrom = []
+    mailContent = []
+    
+    mailIds = []
+    for block in data:
+        mailIds += block.split()
+
+    for i in mailIds:
         status, data = mail.fetch(i, '(RFC822)')
     
-        for response_part in data:
+        for responsePart in data:
             
-            if isinstance(response_part, tuple):
-                message = email.message_from_bytes(response_part[1])
+            if isinstance(responsePart, tuple):
+                message = email.message_from_bytes(responsePart[1])
 
-                mail_from = message['from']
-                mail_subject = message['subject']
+                mailFrom.append(message['from'])
 
                 if message.is_multipart():
-                    mail_content = ''
+                    mailContentContainer = ''
                     for part in message.get_payload():
                         if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
+                            mailContentContainer += part.get_payload()
                 else:
-                    mail_content = message.get_payload()
+                    mailContentContainer = message.get_payload()
+                
+                mailContent.append(mailContentContainer)
 
-    return mail_from, mail_content
+    return mailFrom, mailContent
 
-def send_test_mail(reciver, imageCode):
+def sendTestMail(reciver, imageCode):
     msg = MIMEMultipart()
     msg['Subject'] = '[Email Test]'
     msg['From'] = emailAdress
@@ -90,21 +75,49 @@ def send_test_mail(reciver, imageCode):
 
     image = "Resources/" + str(imageCode) + ".jpg"
 
-    with open(image, 'rb') as fp:
-        img = MIMEImage(fp.read())
-        img.add_header('Content-Disposition', 'attachment', filename="example.jpg")
-        msg.attach(img)
+    if os.path.isfile(image):
+        with open(image, 'rb') as fp:
+            img = MIMEImage(fp.read())
+            img.add_header('Content-Disposition', 'attachment', filename="example.jpg")
+            msg.attach(img)
 
-    try:
-        with smtplib.SMTP(server, port) as smtpObj:
-            smtpObj.ehlo()
-            smtpObj.starttls()
-            smtpObj.login(emailAdress, PASSWORD)
-            smtpObj.sendmail(emailAdress, reciver, msg.as_string())
-            smtpObj.quit()
-    except Exception as e:
-        print(e)    
+        try:
+            with smtplib.SMTP(server, smtpPort) as smtpObj:
+                smtpObj.ehlo()
+                smtpObj.starttls()
+                smtpObj.login(emailAdress, PASSWORD)
+                smtpObj.sendmail(emailAdress, reciver, msg.as_string())
+                smtpObj.quit()
+        except Exception as e:
+            print(e)
+    else:
+        print("Image not exist")
 
-reciver, code = receiveEmail()
-send_test_mail(reciver, int(code))
-# deleteMessage(reciver)
+def representsInt(number):
+    try: 
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+timing = time.time()
+while True:
+    if time.time() - timing > 30:
+
+        mail = imaplib.IMAP4_SSL(imapServer)
+        mail.login(emailAdress, PASSWORD)
+        mail.select('inbox')
+
+        timing = time.time()
+        reciver, code = receiveEmail()
+        
+        if reciver and code:
+            for i in range(len(reciver)):
+                if representsInt(code[i]):
+                    sendTestMail(reciver[i], int(code[i]))
+                else:
+                    print("Code is not numeric")
+        else:
+            print("Lists are empty")
+        
+        deleteMessage()
